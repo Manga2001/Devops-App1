d47696d4c41f1f3a10b3a52e750416ae3ba3ae4b
Cherif Manga <cherifmanga2001@gmail.com>
2024-12-19 07:31:28 +0000

    changement du Message Afficher dans index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
  // Fonction principale pour afficher le message 
 function main() { 
- echo "Hello, GitHub!"; 
+ echo "Hello, EGNABE! Agnil a DIOLA SEMBE"; 
 } 
 // Appel de la fonction principale 
 main(); 
